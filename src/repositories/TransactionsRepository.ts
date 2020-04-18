68203fbfc709557024ddef8e8a08cb79a16f0754
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2020-04-18 17:53:25 -0300

    Challenge finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,31 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce(
+      (total, transaction) =>
+        (transaction.type === 'income' && total + transaction.value) || total,
+      0,
+    );
+
+    const outcome = this.transactions.reduce(
+      (total, transaction) =>
+        (transaction.type === 'outcome' && total + transaction.value) || total,
+      0,
+    );
+
+    const total = income - outcome || 0;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,21 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+    const expectedTotal =
+      type === 'income' ? balance.total + value : balance.total - value;
+
+    if (expectedTotal < 0) {
+      throw Error('Invalid and negative balance');
+    }
+
+    const transation = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transation;
   }
 }
 
